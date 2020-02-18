d560780550e8e4093c6d5331112b03c494e85a9e
Stef Martens <stef.m@hotmail.be>
2020-02-18 14:25:34 +0100

    Aantal functies aangemaakt

--- python/Python_Digi-Glove/Python_Digi-Glove/Python_Digi_Glove.py
+++ python/Python_Digi-Glove/Python_Digi-Glove/Python_Digi_Glove.py
@@ -8,10 +8,21 @@ class POINT(Structure):
 
 
 
-def queryMousePosition():
+def Rechtermuisklik():
     pt = POINT()
     windll.user32.GetCursorPos(byref(pt))
     pyautogui.rightClick(x=pt.x, y=pt.y)
 
+def PaginaSluiten():
+    pyautogui.hotkey('ctrl', 'w')  
 
-queryMousePosition()
+def Kopiëren():
+    pyautogui.hotkey('ctrl', 'c')  
+
+def Plakken():
+    pyautogui.hotkey('ctrl', 'v')
+
+def PrintScreen():
+    pyautogui.hotkey('ctrl', 'PrtSc') 
+
+PrintScreen()
